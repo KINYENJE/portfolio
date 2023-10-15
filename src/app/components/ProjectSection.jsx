e3fad5e55c02f7d2872443bbc997e8d7b46b1a7b
James Njenga <njengajames232@gmail.com>
2023-10-15 17:01:02 +0300

    link addition

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -8,6 +8,8 @@ const projectData = [
         description: "This is an ecommerce website",
         image: '/images/projects/ecommerce.png',
         tag: ["All", "Next.js", "Tailwind CSS"],
+        gitUrl: '/',
+        previewUrl: 'https://kinyenjemart.netlify.app/',
     },
     {
         id: 2,
@@ -15,6 +17,8 @@ const projectData = [
         description: "This is a gym website",
         image: '/images/projects/gym.png',
         tag: ["All"],
+        gitUrl: '/',
+        previewUrl: 'https://kinyenje-gym-site.netlify.app/',
     },
     {
         id: 3,
@@ -22,6 +26,8 @@ const projectData = [
         description: "This is a Nike shoe clone website",
         image: '/images/projects/neki.png',
         tag: ["All", "Vite", "Tailwind CSS"],
+        gitUrl: '/',
+        previewUrl: 'https://kinyenje-neki.netlify.app/',
     },
     {
         id: 4,
@@ -29,6 +35,8 @@ const projectData = [
         description: "This is a Chat GPT Summarizer for articles",
         image: '/images/projects/summarizer.png',
         tag: ["All", "React", "Tailwind CSS"],
+        gitUrl: '/',
+        previewUrl: 'https://kinyenje-ai-summarizer.netlify.app/',
     },
     {
         id: 5,
@@ -36,20 +44,25 @@ const projectData = [
         description: "This is a Chat GPT T-Shirt design generator",
         image: '/images/projects/t-shirt.png',
         tag: ["All", "React"],
+        gitUrl: '/',
+        previewUrl: 'https://kinyenje-ai-shirt.netlify.app/',
     },
 ]
 
 const ProjectSection = () => {
   return (
     <>
-    <h2>My Projects</h2>
-    <div>
+    <h2 className='text-center text-4xl font-bold mt-4 mb-8 md:mb-12'>
+        My Projects</h2>
+    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
         {projectData.map((project) => (
             <ProjectsCard
                 key={project.id}
                 title={project.title}
                 description={project.description}
                 imgUrl={project.image}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
             />
         ))}
         
--- src/app/components/ProjectsCard.jsx
+++ src/app/components/ProjectsCard.jsx
@@ -5,24 +5,32 @@ import React from 'react'
 import { CodeBracketIcon, EyeIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 
-const ProjectsCard = ({imgUrl, title, description }) => {
+const ProjectsCard = ({imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
         <div className='h-52 md:h-72 rounded-t-xl relative group' 
         style={{background: `url(${imgUrl})`, backgroundSize: 'cover'}}>
-            <div className='overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
-                <Link href={'/'} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white relative'>
-                    {/* <CodeBracketIcon  className='h-10 w-10 text-[#ADB7BE] m-2 cursor-pointer hover:white' /> */}
+            <div className='overlay absolute items-center justify-center top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 gap-2'>
+                <Link href={gitUrl} target='blank' className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+                    
                     <svg className = 'h-10 w-10 text-[#ADB7BE]  cursor-pointer hover:white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
                     </svg>
 
                 </Link>
+                <Link href={previewUrl} target='blank' className='h-14 w-14 border-2  rounded-full border-[#ADB7BE] hover:border-white relative'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10 text-[#ADB7BE]  cursor-pointer hover:white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+
+                
+                </Link>
             </div>
 
         </div>
-        <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
+        <div className='text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4'>
             <h5 className='text-xl font-semibold mb-2'>{title}</h5>
             <p className='text-[#ADB7BE]'>{description}</p>
         </div>
